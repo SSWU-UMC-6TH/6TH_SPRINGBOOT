9825854d17e74c220a186d284421ed297d0b8b1d
plum-king <jeesoo0908@gmail.com>
2024-06-16 16:48:55 +0900

    [Feat] 가게 리뷰 목록 조회 API (#12)

--- src/main/java/umc/spring/service/Store/StoreQueryService.java
+++ src/main/java/umc/spring/service/Store/StoreQueryService.java
@@ -1,10 +1,15 @@
 package umc.spring.service.Store;
 
+import org.springframework.data.domain.Page;
 import umc.spring.domain.Region;
+import umc.spring.domain.Review;
 import umc.spring.domain.Store;
 import umc.spring.web.dto.StoreRequestDTO;
 
 public interface StoreQueryService {
 
     Store createStore(StoreRequestDTO.CreateStoreDTO request, Region region);
+
+    Page<Review> getReviewList(Long StoreId, Integer page);
+
 }
--- src/main/java/umc/spring/service/Store/StoreQueryServiceImpl.java
+++ src/main/java/umc/spring/service/Store/StoreQueryServiceImpl.java
@@ -1,14 +1,35 @@
 package umc.spring.service.Store;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import umc.spring.converter.StoreConverter;
 import umc.spring.domain.Region;
+import umc.spring.domain.Review;
 import umc.spring.domain.Store;
+import umc.spring.repository.ReviewRepository;
+import umc.spring.repository.StoreRepository;
 import umc.spring.web.dto.StoreRequestDTO;
 
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class StoreQueryServiceImpl implements StoreQueryService{
+    private final StoreRepository storeRepository;
+    private final ReviewRepository reviewRepository;
     @Override
     public Store createStore(StoreRequestDTO.CreateStoreDTO request, Region region){
         Store newStore = StoreConverter.toStore(request, region);
         return null;
+    }
+
+    @Override
+    public Page<Review> getReviewList(Long StoreId, Integer page) {
+        Store store = storeRepository.findById(StoreId).get();
+
+        Page<Review> StorePage = reviewRepository.findAllByStore(store, PageRequest.of(page, 10));
+        return StorePage;
     }
 }
