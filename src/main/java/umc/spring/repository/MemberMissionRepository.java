672c222193699ded1a0fe38f7067e2b6803a57d4
plum-king <jeesoo0908@gmail.com>
2024-06-16 21:47:49 +0900

    [FEAT] 진행 중인 나의 미션 목록 조회 API 추가 (#17)

--- src/main/java/umc/spring/repository/MemberMissionRepository.java
+++ src/main/java/umc/spring/repository/MemberMissionRepository.java
@@ -1,6 +1,10 @@
 package umc.spring.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import umc.spring.domain.Member;
+import umc.spring.domain.enums.MissionStatus;
 import umc.spring.domain.mapping.MemberMission;
 
 import java.util.List;
@@ -9,4 +13,6 @@ public interface MemberMissionRepository extends JpaRepository<MemberMission, Lo
     List<MemberMission> findByMemberId(Long member);
 
     List<MemberMission> findByMissionId(Long mission);
+
+    Page<MemberMission> findAllByMemberIdAndStatus(Long memberId, MissionStatus missionStatus, PageRequest pageRequest);
 }
